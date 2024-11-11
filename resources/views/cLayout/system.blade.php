b770e84b5ee9cdf9ea36b8fcaf4c4c12081711ca
feyswal <vicentmkota91@gmail.com>
2024-11-12 00:41:56 +0300

    finish

--- resources/views/cLayout/system.blade.php
+++ resources/views/cLayout/system.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Applications</title>
     <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('assets/bootstrap-4.0.0/dist/css/bootstrap.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -15,9 +15,8 @@
     </div>
 
     <!-- Bootstrap JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/js/jquery.js') }}"></script>
+    <script src="{{ asset('assets/bootstrap-4.0.0/dist/js/bootstrap.bundle.js') }}"></script>
 
     @yield("script")
 </body>
